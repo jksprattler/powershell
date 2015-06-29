f0a41c4c360ce8e8160ee19e55beba8840f595e0
jksprattler <jenna.sprattler@gmail.com>
2015-06-28 21:24:56 -0500

    Revert "Revert "test""
    This reverts commit 5130d86908288dfe0323a2dcb46a3fffa5616a28.

---
+++ Get-ADComputer Inventory.ps1
@@ -0,0 +1,7 @@
+﻿###############################################################################
+#This script will pull all machine names, OS, OS version and other properties
+#and export them into a csv file that you can save in your My Docs location
+###############################################################################
+
+Get-ADComputer -Filter * -Properties Name, operatingSystem, operatingSystemVersion |  
+Export-Csv "\\ServerName\MyDoc$\UserName\My Documents\Documentation\Asset Inventory\Get-ADComputer.csv" -NoTypeInformation
