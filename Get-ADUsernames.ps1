5130d86908288dfe0323a2dcb46a3fffa5616a28
jksprattler <jenna.sprattler@gmail.com>
2015-06-28 21:23:43 -0500

    Revert "test"
    This reverts commit 04386d47f94f3b102b2e3f68b5bef4e7d669d27a.

--- Get-ADUsernames.ps1
+++
@@ -1,8 +0,0 @@
-﻿###############################################################################
-#This script pulls all the users from AD and exports them to a csv file
-###############################################################################
-
-Get-ADUser -Filter * | 
-select name,samaccountname |  
-Sort-Object name |
-Export-Csv "\\ServerName\MyDoc$\UserName\My Documents\Documentation\AD usernames.csv" -NoTypeInformation
--- Phone Numbers/Set-ADUser phone numbers import.ps1
+++
@@ -1,9 +0,0 @@
-﻿###############################################################################
-#This script imports the edited phone numbers from the Get-ADUser phone number 
-#export script and imports the changes back into AD
-###############################################################################
-
-Import-Csv "\\ServerName\MyDoc$\UserName\My Documents\Documentation\AD phone numbers.csv" |
-ForEach-Object {
-Set-ADUser -Identity $_.samaccountname -Replace @{telephonenumber=$_.OfficePhone;mobile=$_.MobilePhone;info=$_.info}
-}
