9140f545efe094548672c863e06b1b512f0f0879
jksprattler <jenna.sprattler@gmail.com>
2015-06-28 21:35:58 -0500

    Extract all AD usernames to csv file

---
+++ Get-ADUsernames.ps1
@@ -0,0 +1,8 @@
+﻿###############################################################################
+#This script pulls all the users from AD and exports them to a csv file
+###############################################################################
+
+Get-ADUser -Filter * | 
+select name,samaccountname |  
+Sort-Object name |
+Export-Csv "\\ServerName\MyDoc$\UserName\My Documents\Documentation\AD usernames.csv" -NoTypeInformation
