8008e7afe5b10d1a797604104aa6cdf8f301ca51
jksprattler <jenna.sprattler@gmail.com>
2015-06-28 21:27:08 -0500

    Revert "Revert "Revert "test"""
    This reverts commit f0a41c4c360ce8e8160ee19e55beba8840f595e0.

--- Phone Numbers/Get-ADUser phone number export.ps1
+++
@@ -1,9 +0,0 @@
-﻿###############################################################################
-#This script will pull all the phone numbers from the AD Domain and export
-#them into a csv file in My Docs named, "AD phone numbers"
-###############################################################################
-
-Get-ADUser  -Filter * -Properties OfficePhone,MobilePhone,info |
-select name,samaccountname,OfficePhone,MobilePhone,info |  
-Sort-Object name |
-Export-Csv "\\ServerName\MyDoc$\UserName\My Documents\Documentation\AD phone numbers.csv" -NoTypeInformation
--- Phone Numbers/clear phone numbers.ps1
+++
@@ -1,5 +0,0 @@
-﻿###############################################################################
-#This script can be used to clear AD user phone numbers
-###############################################################################
-
-Set-ADUser -Identity UserName -clear mobile,info
